e79420f7f93066f1c66f00e4c4275533c1a17c33
E-SeongHO <lsh031255@gmai.com>
2021-07-08 22:16:14 +0900

    implement : title scene done

--- Assets/Scripts/TitleScene/TitleManage.cs
+++ Assets/Scripts/TitleScene/TitleManage.cs
@@ -13,23 +13,34 @@ public class TitleManage : MonoBehaviour
 
     private Animator logo_animator;
     private Animator car_animator;
+    private Animator player1_animator;
 
-    //public Image logo;
     public Text press_any_key;
-    //private bool show_logo;
+    public GameObject player1;
+
+    // player 생성 위치 (Car 로부터 거리)
+    private float player_xdiff = 1202.6f;
+    private float player_ydiff = -173f;
+    
 
     private void Start()
     {
+        // find objects
         distant_view = GameObject.Find("Title_Distant");
         front_view = GameObject.Find("Title_Front");
         car = GameObject.Find("Title_Car");
         logo = GameObject.Find("Title_Logo");
         
+        // set active false
         press_any_key.gameObject.SetActive(false);
+        player1.SetActive(false);
+
         // get animator component
         logo_animator = logo.GetComponent<Animator>();
         car_animator = car.GetComponent<Animator>();
-        // logo play
+        player1_animator = player1.GetComponent<Animator>();
+
+        // play logo
         logo_animator.SetBool("play", true);
     }
 
@@ -48,13 +59,25 @@ public class TitleManage : MonoBehaviour
                 logo.SetActive(false);
                 press_any_key.gameObject.SetActive(false);
 
-                StartCoroutine("StopCar");
+                StartCoroutine("StopCar"); // when coroutine end, start camera move in LateUpdate 
 
                 // stop background moving
                 distant_view.GetComponent<DistantViewMove_Title>().enabled = false;
                 front_view.GetComponent<FrontViewMove_Title>().enabled = false;   
             }
         }
+
+        if (car_animator.GetCurrentAnimatorStateInfo(0).IsName("Car_Off")
+            && car_animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9f
+            && car_animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.95f) 
+        {
+            Vector3 pos = new Vector3(car.transform.position.x + player_xdiff, car.transform.position.y + player_ydiff, 0);
+            player1.transform.position = pos;
+            player1.SetActive(true);
+
+            StartCoroutine("MovePlayer1");
+        }
+        
     }
     // Camera Move
     private void LateUpdate()
@@ -92,10 +115,22 @@ public class TitleManage : MonoBehaviour
 
         // open animation
         car_animator.SetBool("open", true);
-
-        // create player
-        
+       
         yield return null;
     }
 
+    IEnumerator MovePlayer1()
+    {
+        float speed = 10f;
+        while(player1.transform.position.x < 1050) // 700(camera) + 350(적정위치)
+        {
+            player1.transform.Translate(speed * Time.deltaTime, 0, 0);
+            yield return new WaitForFixedUpdate();
+        }
+
+        // idle animation
+        player1_animator.SetBool("stop", true);
+
+        yield return null;
+    }
 }
