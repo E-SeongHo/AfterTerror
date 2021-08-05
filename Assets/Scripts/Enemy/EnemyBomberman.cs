9d94d4ec8946600470a5b559a47ed7a4620e8865
E-SeongHo <lsh031255@naver.com>
2021-08-06 01:12:32 +0900

    implement : EnemyBomberman 구현

---
+++ Assets/Scripts/Enemy/EnemyBomberman.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBomberman : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 2;
+    [SerializeField] private int throwbombbound = 3;
+
+    // throw dynamite... 
+    [SerializeField] private GameObject dynamite_prefab;
+    private float autoThrowTime = 4f;
+    private float count;
+
+    private EnemyController controller;
+    private Animator animator;
+
+    private void Start()
+    {
+        controller = GetComponent<EnemyController>();
+        controller.SetCurrentHealth(maxHealth);
+        controller.SetMaxHealth(maxHealth);
+        animator = GetComponent<Animator>();
+
+        // initial fire time is random
+        count = Random.Range(0.5f, 1.5f);
+    }
+    private void FixedUpdate()
+    {
+        if (controller.GetInteractionState())
+        {
+            AutoThrowProcess();
+            HitThrowProcess();
+        }
+        if (controller.GetRunState() && !controller.GetDieState())
+        {
+            controller.StartCoroutine("RunAwayProcess");
+        }
+    }
+    private void ThrowDynamite()
+    {
+        animator.SetTrigger("throw");
+        Vector3 init_pos = gameObject.transform.position + new Vector3(30f, 0, 0);
+        GameObject newDynamite = Instantiate(dynamite_prefab, init_pos, Quaternion.identity);
+    }
+    private void AutoThrowProcess()
+    {
+        count -= Time.deltaTime;
+        if(count < 0)
+        {
+            ThrowDynamite();
+            // after first shot, next fire is after 4f(autoShotTime) time
+            count = autoThrowTime;
+        }
+    }
+    // 플레이어가 공격을 실패하면 카운트 후 공격
+    private void HitThrowProcess()
+    {
+        if (controller.GetAttackCount() >= throwbombbound)
+        {
+            ThrowDynamite();
+            controller.ResetAttackCount();
+        }
+    }
+    
+}
